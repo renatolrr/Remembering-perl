cd86fcc592da56bdc4bf8aef62d8a23fc231f971
renatolrr <renatolrr@gmail.com>
2015-09-29 17:37:29 +0200

    Añadiendo 1_5.pl

---
+++ perlmaven/1_5.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+print "Run: perl mycode.pl", "\n";
+print "Debuger: perl -d mycode.pl", "\n";
+print "Intalling Devel::ptkdb: perl -d:ptkdb mycode.pl", "\n";
+
