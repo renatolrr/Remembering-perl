ee417c31728b646e5c59c49ed997637c50e22b02
renatolrr <renatolrr@gmail.com>
2015-09-30 22:32:16 +0200

    Añadiendo 2.pl

---
+++ begining perl/2.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+print "CPAN http://www.cpan.org", "\n"; 
+print "CPAN.pm", "\n"; 
+print "App::cpanminus", "\n"; 
+print "PPM witht ActivePerl.", "\n";
+print "CPAN::Mini Local CPAN mirror.", "\n";
